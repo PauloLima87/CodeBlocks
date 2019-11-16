e79b935d8603f077864556cd5d73570346dd14d5
Paulo Henrique Maia de Lima <zdesenvolvimento@gmail.com>
2019-11-16 13:21:12 -0300

    Aula13

--- Aula 13 - Como Utilizar do while.c
+++ Aula 13 - Como Utilizar do while.c
@@ -6,8 +6,16 @@
 int main (int argc, char *argv[])
 {
   setlocale(LC_ALL, "Portuguese"); //aceitar caracteres especiais e pontuação em PT-BR
+  int i=0;
 
+  do
+  {
+      printf("Teste 2\n");
+  } while (i!=0);
 
+  while (i!=0){
+    printf("Teste 1\n");
+  };
 
   system("PAUSE");
 return 0;
