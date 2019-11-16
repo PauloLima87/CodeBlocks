61762192f92f97c7fd0ab7450f663ab7684d003f
Paulo Henrique Maia de Lima <zdesenvolvimento@gmail.com>
2019-11-16 13:25:16 -0300

    Testes de laços Do..While e While

--- Aula 13 - Como Utilizar do while.c
+++ Aula 13 - Como Utilizar do while.c
@@ -8,14 +8,16 @@ int main (int argc, char *argv[])
   setlocale(LC_ALL, "Portuguese"); //aceitar caracteres especiais e pontuação em PT-BR
   int i=0;
 
-  do
-  {
-      printf("Teste 2\n");
-  } while (i!=0);
-
-  while (i!=0){
+   while (i!=0){ //primeiro confere depois entra nos comandos
     printf("Teste 1\n");
   };
+
+  do //executa e depois confere condições
+  {
+      printf("Teste 2\n");
+  } while (i!=0); //vai executar pelo menos uma vez
+
+
 
   system("PAUSE");
 return 0;
