da962d66b3c1fe166c458b085ecb67a6595832aa
Paulo Henrique Maia de Lima <zdesenvolvimento@gmail.com>
2019-11-20 16:35:44 -0300

    Estudo

--- Aula 16 - Switch.c
+++ Aula 16 - Switch.c
@@ -6,20 +6,38 @@
 int main (int argc, char *argv[])
 {
   setlocale(LC_ALL, "Portuguese"); //aceitar caracteres especiais e pontuação em PT-BR
-  int idade;
+  int num;
 
-  printf("Favor informar idade: ");
-  scanf("%i", &idade);
-  if (idade<=5)
-    printf("Bebê\n\n");
-  else if (5 < idade && idade <=10)
-    printf("Criança\n\n");
-  else if (10 < idade && idade <=17)
-    printf("Adolecente\n\n");
-  else if (17< idade && idade<65)
-    printf("Adulto\n\n");
-  else
-    printf("Idoso\n\n");
+ while (num>0 &&num<=5){
+  printf("Favor informar número de 1 a 5 : ");
+  scanf("%i", &num);
+  switch (num){
+  case 1:
+    printf("Primeiro\n\n");
+    break; //se nao colocar passar por todos
+
+  case 2:
+    printf("Segundo\n\n");
+    break; //se nao colocar passar por todos
+
+  case 3:
+    printf("Terceiro\n\n");
+    break; //se nao colocar passar por todos
+
+  case 4:
+    printf("Quarto\n\n");
+    break; //se nao colocar passar por todos
+
+  case 5:
+    printf("Quinto\n\n");
+    break; //se nao colocar passar por todos
+
+  default:
+    printf("Opção Inválida\n\n");
+    break; //se nao colocar passar por todos
+
+  }
+  };
 
 
   system("PAUSE");
