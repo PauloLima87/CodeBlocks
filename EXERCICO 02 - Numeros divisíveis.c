3cd107bb16ea1e79b44b011d051da4dee204ed44
Paulo Henrique Maia de Lima <zdesenvolvimento@gmail.com>
2019-11-21 12:55:46 -0300

    Lista de Exercícios Facebook

--- EXERCICO 02 - Numeros divisíveis.c
+++ EXERCICO 02 - Numeros divisíveis.c
@@ -11,17 +11,22 @@ int main (int argc, char *argv[])
 {
   setlocale(LC_ALL, "Portuguese"); //aceitar caracteres especiais e pontuação em PT-BR
   int num1, num2, res;
-  printf("Digite o primeiro valor: ");
-  scanf("%i", &num1);
-  printf("Digite o segundo valor: ");
-  scanf("%i", &num2);
-  res = num1 % num2;
-
-  if (res==0)
-    printf("O Numero %i É divisível por %i", num1, num2);
-  else
-    printf("O Numero %i NÃO É divisível por %i", num1, num2);
-
+    printf("Digite o primeiro valor: ");
+    scanf("%i", &num1);
+    printf("Digite o segundo valor: ");
+    scanf("%i", &num2);
+     if ( num2 == 0){
+    printf("Impossível dividir por 0 (zero) ");}
+    res = num1 % num2;
+  //if ( num2 == 0){
+  //  printf("Impossível dividir por 0 (zero) ");}
+  //else{
+    if (res==0){
+      printf("O Numero %i É divisível por %i", num1, num2);
+    } else {
+      printf("O Numero %i NÃO É divisível por %i", num1, num2);
+    }
+    //}
   printf("\nNumero 1: %i \nNumero 2: %i\n\n", num1, num2);
 
   system("PAUSE");
