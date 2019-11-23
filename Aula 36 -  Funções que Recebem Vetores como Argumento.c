ccc1e183b2dfee2bd7e9dbf1e10e59640d9ebe9b
Paulo Henrique Maia de Lima <zdesenvolvimento@gmail.com>
2019-11-23 11:30:00 -0300

    Estudo Mestrado

--- Aula 36 -  Funções que Recebem Vetores como Argumento.c
+++ Aula 36 -  Funções que Recebem Vetores como Argumento.c
@@ -8,6 +8,32 @@ int main (int argc, char *argv[])
 {
   setlocale(LC_ALL, "Portuguese"); //aceitar caracteres especiais e pontuação em PT-BR
 
+  int vetor[10] ={6,4,1,9,8,0,5,7,3,2};
+  int aux;
+
+  void ordemCrescente(int cadeia[], int tam);
+
+  ordemCrescente(vetor,10);//passar vetor nao necessita tem o indice, pode passar so o nome
+  for (int i = 0; i<10 ;i++)
+  printf("%i", vetor[i]);
 
   return 0;
+}
+
+void ordemCrescente(int cadeia[], int tam)
+{
+    int tmp;
+    for (int i=0;i<tam ;i++ )
+        {
+        for (int j=i+1; j<tam ;j++ )
+            {
+              if (cadeia[i]>cadeia[j])
+              {
+                    tmp = cadeia[i];
+                    cadeia[i]= cadeia[j];
+                    cadeia[j]=tmp;
+              }
+            }
+        };
+    //semreturn ALGUMA COISA, função definida como VOID
 }
