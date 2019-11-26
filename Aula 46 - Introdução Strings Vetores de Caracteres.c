74bde9ffbc3a7f026aa8265514210d64d24a9785
Paulo Henrique Maia de Lima <zdesenvolvimento@gmail.com>
2019-11-26 01:32:39 -0300

    Estudo Mestrado

--- Aula 46 - Introdução Strings Vetores de Caracteres.c
+++ Aula 46 - Introdução Strings Vetores de Caracteres.c
@@ -3,15 +3,16 @@
 #include <conio.h>
 #include <locale.h>
 
+//recuperado
 int main (int argc, char *argv[])
 {
   setlocale(LC_ALL, "Portuguese"); //aceitar caracteres especiais e pontua��o em PT-BR
 
-  char teste[] = {'B','R','A','S','I','L'};
+  char teste[] = {'B','R','A','S','I','L'};//digitando caracteres
 
-  for (int i = 0;i < 6 ; i++)
+  for (int i = 0;i < 6 ; i++) //for para apresentação da cadeia
       {
-        printf("%c", teste[i]);
+        printf("%c", teste[i]); //print da cadeia
       };
 
   return 0;
