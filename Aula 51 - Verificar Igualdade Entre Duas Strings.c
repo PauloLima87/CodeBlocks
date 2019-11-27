1d63f91733d3755b36da5cf440f01a6d305b7413
Paulo Henrique Maia de Lima <zdesenvolvimento@gmail.com>
2019-11-26 23:43:37 -0300

    Estudo Mestrado

---
+++ Aula 51 - Verificar Igualdade Entre Duas Strings.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <locale.h>
+
+int main (int argc, char *argv[])
+{
+  setlocale(LC_ALL, "Portuguese"); //aceitar caracteres especiais e pontuação em PT-BR
+
+   return 0;
+}
