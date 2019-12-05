4e912f830f0148b6defc37e01860738961fe0ef4
Paulo Henrique Maia de Lima <zdesenvolvimento@gmail.com>
2019-12-05 12:39:57 -0300

    Estudo Mestrado

--- Aula 54 - Verificar Caracteres Alfabéticos - Apenas Letras - lógica.c
+++ Aula 54 - Verificar Caracteres Alfabéticos - Apenas Letras - lógica.c
@@ -9,12 +9,30 @@ int main (int argc, char *argv[])
     setlocale(LC_ALL, "Portuguese"); //aceitar caracteres especiais e pontuação em PT-BR
 
     void verificaLetra(char stringCadeia);
+    char nome[30];
+    int aux = 0;
 
+    //entrada da cadeia a ser verificada
+    printf("Digite o Nome: ");
+    scanf("%s", nome);
+
+    while (nome[aux]!='\0')
+        {
+            verificaLetra(nome[aux]);
+            ++aux;
+        }
 
 system("PAUSE");
 return 0;
 }
 void verificaLetra(char stringCadeia)
 {
-    int
+    if ((stringCadeia>='a' && stringCadeia<='z')||
+        (stringCadeia>='A' && stringCadeia<='Z'))
+    {
+        printf("É Letra\n");
+    }else
+    {
+        printf("Não é Letra\n");
+    }
 }
